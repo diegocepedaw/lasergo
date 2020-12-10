a45bcf6c4dc2477b77b36eff465f1d9340c59807
diegocepedaw <diegocepedaw@gmail.com>
2020-12-10 01:32:39 -0800

    detect and remove noise intersections

--- gridDetect.py
+++ gridDetect.py
@@ -100,23 +100,60 @@ def  process_analysis_grid(squareFile):
     img = np.zeros((src.shape[0],src.shape[1],3), np.uint8)
     ### drawing rectangle for each contour for visualising
     transparent_background = np.zeros((570, 580, 4))
+    coord_list =[]
+
+    # remove any extra points by searching for points that have too close a neighbor and removing them
     for cnt in contours:
         x,y,w,h=cnt
         #cv.rectangle(src,(x,y),(x+w,y+h),(0,255,0),2)
 
         center_coords = (int((x+w/2)), int((y+h/2)))
+        coord_list.append(center_coords)
+        cv.circle(transparent_background, center_coords, 5, (255, 255, 255), -1)
+    show_wait_destroy("intersections", transparent_background)
+
+    # remove any extra points by searching for points that have too close a neighbor and removing them
+    pared_coords = []
+    # sort coordinates by x axis
+    coord_list_i = sorted(coord_list, key = lambda x: x[0])
+    coord_listy = sorted(coord_list, key = lambda x: x[0])
+    rejected_coords = set()
+    print(coord_list)
+    for icoord in coord_list_i:
+        for jcoord in coord_listy:
+            if icoord == jcoord or jcoord in rejected_coords:
+                continue
+            if abs(icoord[0] - jcoord[0]) < 20:
+                if abs(icoord[1] - jcoord[1]) < 20:
+                    print(icoord,jcoord)
+                    coord_list_i.remove(jcoord)
+                    coord_listy.remove(jcoord)
+                    rejected_coords.add(jcoord)
+                    break
+        pared_coords.append(icoord)
+        
+    print(len(contours))
+    print(len(pared_coords))
+
+    grid_background = np.zeros((570, 580, 4))
+    for coord in pared_coords:
+        x,y = coord
+        #cv.rectangle(src,(x,y),(x+w,y+h),(0,255,0),2)
+
         # make a 60px square cocentric with the contour
         # this represents each individual area that will be evaulated
-        cv.rectangle(src,(center_coords[0]-15,center_coords[1]-15),(center_coords[0]+15,center_coords[1]+15),(128,0,128),2)
-        cv.circle(src, center_coords, 3, (255, 255, 255), -1)
+        cv.rectangle(src,(x-15,y-15),(x+15,y+15),(128,0,128),2)
+        cv.circle(src, coord, 3, (255, 255, 255), -1)
 
-        cv.circle(transparent_background, center_coords, 5, (255, 255, 255), -1)
+        cv.circle(transparent_background, coord, 5, (255, 0, 255), -1)
+        cv.circle(grid_background, coord, 5, (255, 255, 255), -1)
     
     show_wait_destroy("intersections", src)
     show_wait_destroy("intersections", transparent_background)
+    
 
     filename = 'gridfiles/evaluation_grid.png'
-    cv.imwrite(filename, transparent_background) 
+    cv.imwrite(filename, grid_background) 
     
     return 0
 if __name__ == "__main__":
